36d4d73b727ac060c190d952d1f85712b95672ed
Javohir <qayumovj061998@gmail.com>
2021-11-28 23:17:20 +0500

    Initial commit and changed

--- app/src/main/java/uz/pdp/breakingnews/dagger/retro/ApiService.kt
+++ app/src/main/java/uz/pdp/breakingnews/dagger/retro/ApiService.kt
@@ -7,12 +7,13 @@ import uz.pdp.breakingnews.madels.news.NewsResponse
 
 interface ApiService {
 
-    @GET("everything?q=news&apiKey=8d5179f81da64dac8f0aefc43bbf5fac")
+    @GET("everything?q=news&apiKey=6111561047d7436a9b83229b7cbb927f")
     suspend fun getNewsRetro(): Response<NewsResponse>
 
-    @GET("everything?apiKey=8d5179f81da64dac8f0aefc43bbf5fac")
+    @GET("everything?apiKey=6111561047d7436a9b83229b7cbb927f")
     suspend fun getSearchRetro(@Query("q") q: String): Response<NewsResponse>
 
-    @GET("top-headlines?country=us&apiKey=8d5179f81da64dac8f0aefc43bbf5fac")
+    @GET("top-headlines?country=ru&apiKey=6111561047d7436a9b83229b7cbb927f")
     suspend fun getCategoryRetro(@Query("category")  category: String): Response<NewsResponse>
 }
+//6111561047d7436a9b83229b7cbb927f
